8769fd81db7e164ca0bb8088e4426bfeacb0b5b8
v-huangyafeng3 <v-huangyafeng3@xiaomi.com>
2026-03-23 01:12:58 +0800

    feat: 重构应用结构并添加路由支持
    - 更新 index.html，移除静态内容并添加根元素
    - 在 renderer.tsx 中引入 React Router 和 Suspense，设置路由提供者
    - 新增 constants.tsx 文件，定义菜单项和用户下拉菜单
    - 更新 App 组件，使用 Ant Design 的 Layout 组件结构
    - 新增 LoadingFallback 组件，提供加载状态的视觉反馈
    - 添加 react-dom/client 类型定义文件以支持 createRoot 方法

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import { Outlet } from "react-router-dom";
+
+import { Layout } from "antd";
+const { Header, Sider, Content } = Layout;
+
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <Layout className="asp-comprehension-home" style={{ height: "100vh", overflow: "hidden" }}>
+      <Sider
+        collapsible
+        breakpoint="md"
+        className="asp-comprehension-home-menu"
+        collapsed={false}
+        collapsedWidth={80}
+        trigger={null}
+        width={240}
+      />
+
+      <Layout style={{ height: "100%", overflow: "hidden" }}>
+        <Header />
+        <Content className="asp-comprehension-home-content">
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default App;
